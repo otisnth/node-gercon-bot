72ed0003f7f9caf9d8abf250b90ea09d18ad07ce
otisnth <otisnth@gmail.com>
2024-03-09 11:15:07 +0300

    реализован просмотр детальной инцидента

--- src/controllers/history/action.js
+++ src/controllers/history/action.js
@@ -1,5 +1,7 @@
 import Incident from "../../models/Incident.js";
 import TypeInjury from "../../models/TypeInjury.js";
+import Room from "../../models/Room.js";
+import Order from "../../models/Order.js";
 
 export async function getHistory(ctx) {
     let data;
@@ -32,3 +34,49 @@ export function getPageHistory(data, itemPerPage, currentPage) {
     const endIndex = startIndex + itemPerPage;
     return data.length < endIndex ? data.slice(startIndex) : data.slice(startIndex, itemPerPage);
 }
+
+export const historyItemAction = async (ctx) => {
+    const data = JSON.parse(ctx.callbackQuery.data);
+    let item = await Incident.findByPk(data.id);
+    item = item.dataValues;
+    let injury = await TypeInjury.findByPk(item.injury);
+    item.injury = injury.name;
+    if (item.room !== null) {
+        let room = await Room.findByPk(item.room);
+        item.location = room.name;
+        item.address = room.address;
+    } else {
+        let order = await Order.findByPk(item.order);
+        item.location = order.name;
+        item.address = order.address;
+        item.orderDate = order.date;
+        item.orderDescription = order.description;
+    }
+
+    if (item.room !== null) {
+        await ctx.reply(
+            ctx.i18n.t("scenes.history.detail.room", {
+                name: item.location,
+                address: item.address,
+                fullName: item.fullName,
+                date: item.date,
+                injury: item.injury,
+                description: item.description,
+            })
+        );
+    } else {
+        await ctx.reply(
+            ctx.i18n.t("scenes.history.detail.order", {
+                name: item.location,
+                address: item.address,
+                fullName: item.fullName,
+                date: item.date,
+                injury: item.injury,
+                description: item.description,
+                orderDate: item.orderDate,
+                orderDescription: item.orderDescription,
+            })
+        );
+    }
+    return;
+};
--- src/controllers/history/index.js
+++ src/controllers/history/index.js
@@ -1,7 +1,7 @@
 import { Scenes } from "telegraf";
 import TelegrafI18n from "telegraf-i18n";
 import { getHistoryKeyboard, getPagination } from "./helpers.js";
-import { getHistory, getPageHistory } from "./action.js";
+import { getHistory, getPageHistory, historyItemAction } from "./action.js";
 
 const history = new Scenes.BaseScene("history");
 
@@ -43,6 +43,19 @@ history.hears(TelegrafI18n.match("keyboards.paginationKeyboard.prev"), async (ct
     }
     paginationMsgs = [];
     currentPage--;
+    const historyKeyboard = await getHistoryKeyboard(ctx, getPageHistory(historyList, itemPerPage, currentPage));
+    let msg = await ctx.reply(ctx.i18n.t("keyboards.historyKeyboard.title"), historyKeyboard);
+    paginationMsgs.push(msg);
+    let showed = await getPagination(ctx, itemPerPage, currentPage, historyList.length);
+    paginationMsgs.push(showed);
+});
+
+history.action(/getHistoryItem/, async (ctx) => {
+    for (const i of paginationMsgs) {
+        await ctx.deleteMessage(i.message_id);
+    }
+    paginationMsgs = [];
+    await historyItemAction(ctx);
     const historyKeyboard = await getHistoryKeyboard(ctx, getPageHistory(historyList, itemPerPage, currentPage));
     let msg = await ctx.reply(ctx.i18n.t("keyboards.historyKeyboard.title"), historyKeyboard);
     paginationMsgs.push(msg);
